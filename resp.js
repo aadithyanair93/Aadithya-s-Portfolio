851f013c3ee28dd9458dc84418deec4401aa89b3
aadithyanair93 <131964464+aadithyanair93@users.noreply.github.com>
2023-08-08 18:08:26 +0530

    Done

---
+++ resp.js
@@ -0,0 +1,7 @@
+const ham = document.querySelector(."ham");
+const right = document.querySelector(."right");
+
+line.addEventListener("click",() =>{
+    ham.classList.toggle("active");
+    right.classList.toggle("active");
+})
